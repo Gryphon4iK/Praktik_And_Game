4a497d349c11a6847d6ac47809fdcbd606348712
Vladislav <158111194+Gryphon4iK@users.noreply.github.com>
2024-09-29 00:32:28 +0400

    Add files via upload

---
+++ WindowMessage/WindowMessage/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowMessage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Вариантов нет, всего одна кнопка :(",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+            textBox1.Text = "Вы нажали ОК";
+            this.TopMost = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Выберите один из вариантов",
+                "Сообщение",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+            if (result == DialogResult.Yes)
+                textBox1.Text = "Вы нажали ДА";
+            else
+                textBox1.Text = "Вы нажали НЕТ";
+            this.TopMost = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult = MessageBox.Show(
+                "Выберите один из вариантов",
+                "Сообщение",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+
+            if (DialogResult == DialogResult.Yes)
+                textBox1.Text = "Вы нажали ДА";
+            if (DialogResult == DialogResult.No)
+                textBox1.Text = "Вы нажали НЕТ";
+            if (DialogResult == DialogResult.Cancel)
+                textBox1.Text = "Вы нажали ОТМЕНА";
+            this.TopMost = true;
+        }
+    }
+}
